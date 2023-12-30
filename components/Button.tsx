a525d96baaedf2cfef19038009f9e3876b1862f4
LyhourMam <144458914+LyhuaMam@users.noreply.github.com>
2023-12-30 09:16:25 +0700

    update button cpn and fix layout recipes

--- components/Button.tsx
+++ components/Button.tsx
@@ -5,10 +5,10 @@ import { FaArrowRight } from "react-icons/fa6";
 type ButtonPropsTypes = {
   children: React.ReactNode;
   size?: "xs" | "sm" | "md" | "lg" | "xl" | "landscape";
-  bgColor?: "gradient-blue" | "white";
+  bgColor?: "gradient-blue" | "white" | string;
   radius?: "sm" | "md" | "lg" | "xl" | "full";
   showArrow?: boolean;
-  textColor?: "black";
+  textColor?: string;
   hover?: boolean;
   uppercase?: boolean;
   className?: string;
@@ -21,30 +21,16 @@ export default function Button(props: ButtonPropsTypes) {
     <button
       className={cn(
         ` cursor-pointer  group rounded-lg text-white px-3 py-2 transition-all duration-150 ease-in-out bg-gradient-pink border active:border border-black border-r-4 border-b-4 ${props.className}`,
-        {
-          uppercase: props.uppercase,
-          "rounded-sm": props.radius === "sm",
-          "rounded-md": props.radius === "md",
-          "rounded-lg": props.radius === "lg",
-          "rounded-xl": props.radius === "xl",
-          "rounded-full": props.radius === "full",
-          "py-1 px-3 sm:py-1 sm:px-4 lg:px-5  text-sm sm:text-lg gap-1":
-            props.size === "landscape",
-          "p-1 sm:p-2 lg:px-4 lg:py-1 text-sm sm:text-lg gap-1":
-            props.size === "xs",
-          "py-2 px-2 sm:p-2 lg:px-3 lg:py-2 text-base sm:text-lg lg:text-xl gap-1":
-            props.size === "sm",
-          "px-2 py-1 sm:px-4 sm:py-2 lg:px-4 lg:py-3 text-xl lg:text-2xl gap-1 lg:gap-2":
-            props.size === "md",
-          "p-2 sm:px-4 sm:py-3 lg:px-6 lg:py-4 text-xl sm:text-2xl lg:text-3xl gap-1 sm:gap-3":
-            props.size === "lg",
-          "px-3 py-2 sm:px-4 sm:py-3 lg:px-7 lg:py-7 text-xl sm:text-3xl lg:text-5xl gap-2 md:gap-4":
-            props.size === "xl",
-          "flex items-center justify-center": props.showArrow,
-          "bg-gradient-blue": props.bgColor === "gradient-blue",
-          "bg-white text-black": props.bgColor === "white",
-          "text-black": props.textColor === "black",
-        }
+        props.radius && `rounded-${props.radius}`,
+        props.size === "xs" && "p-1 sm:p-2 lg:px-4 lg:py-1 text-sm sm:text-lg gap-1",
+        props.size === "sm" && "py-2 px-2 sm:p-2 lg:px-3 lg:py-2 text-base sm:text-lg lg:text-xl gap-1",
+        props.size === "md" && "px-2 py-1 sm:px-4 sm:py-2 lg:px-4 lg:py-3 text-xl lg:text-2xl gap-1 lg:gap-2",
+        props.size === "lg" && "p-2 sm:px-4 sm:py-3 lg:px-6 lg:py-4 text-xl sm:text-2xl lg:text-3xl gap-1 sm:gap-3",
+        props.size === "xl" && "px-3 py-2 sm:px-4 sm:py-3 lg:px-7 lg:py-7 text-xl sm:text-3xl lg:text-5xl gap-2 md:gap-4",
+        props.size === "landscape" && "py-1 px-3 sm:py-1 sm:px-4 lg:px-5  text-sm sm:text-lg gap-1",
+        props.showArrow && "flex items-center justify-center",
+        props.bgColor === "gradient-blue" ? "bg-gradient-blue" : `bg-${props.bgColor}`,
+        props.textColor === `${props.textColor}`,
       )}
       onClick={props.onClick}
     >
@@ -52,6 +38,7 @@ export default function Button(props: ButtonPropsTypes) {
         className={cn(
           `text-${props.textColor} duration-500 
       `,
+          props.bgColor === "white" && "text-black",
           {
             "group-hover:text-white":
               props.textColor === "black" && props.hover,
@@ -63,11 +50,13 @@ export default function Button(props: ButtonPropsTypes) {
       </span>
       {props.showArrow && (
         <FaArrowRight
-          className={cn(` duration-500 group-hover:translate-x-1`, {
-            "group-hover:text-white ":
-              props.hover && props.textColor === "black",
-            "group-hover:text-black ": props.hover && !props.textColor,
-          })}
+          className={cn(` duration-500 group-hover:translate-x-1`,
+            props.textColor && `text-${props.textColor}`,
+            {
+              "group-hover:text-white ":
+                props.hover && props.textColor === "black",
+              "group-hover:text-black ": props.hover && !props.textColor,
+            })}
         />
       )}
     </button>
--- components/pages/PageCook/CookPage.tsx
+++ components/pages/PageCook/CookPage.tsx
@@ -2,7 +2,6 @@
 
 import Button from "@/components/Button";
 import Menu from "@/components/Menu";
-import RecipePage from "./RecipePage";
 import { Ingredient, Ingredients } from "@/constant/const";
 import { cn } from "@/lib/utils";
 import Image from "next/image";
@@ -10,22 +9,23 @@ import Link from "next/link";
 import React, { useEffect, useRef, useState } from "react";
 import { IoSearch } from "react-icons/io5";
 import { Box } from "lucide-react";
+import RecipesPage from "./RecipesPage";
 
 export default function CookPage() {
 
     const searchRef = useRef<HTMLDivElement | null>(null)
-    const [isViewSelectedPage,setViewSelectedPage] = useState(false)
-    const [isOpen,setOpen] = useState(false)
+    const [isViewSelectedPage, setViewSelectedPage] = useState(false)
+    const [isOpen, setOpen] = useState(false)
     const [allIngredients, setAllIngredients] = useState(Ingredients);
     const [switchSearch, setSwitchSearch] = useState(true);
     const [searchValue, setSearchValue] = useState("");
-    const [isRemoveAllOpen,setRemoveAllOpen] = useState(false);
-    
-    const selectedIngredient = allIngredients.map(category=>category.selected).flat()
+    const [isRemoveAllOpen, setRemoveAllOpen] = useState(false);
 
-    useEffect(()=>{
-        const handleClickOutside = (e:any) => {
-            if(searchRef.current && !searchRef.current.contains(e.target)){
+    const selectedIngredient = allIngredients.map(category => category.selected).flat()
+
+    useEffect(() => {
+        const handleClickOutside = (e: any) => {
+            if (searchRef.current && !searchRef.current.contains(e.target)) {
                 setOpen(false)
             }
         }
@@ -33,7 +33,7 @@ export default function CookPage() {
         document.addEventListener('click', handleClickOutside);
 
         return () => {
-        document.removeEventListener('click', handleClickOutside);
+            document.removeEventListener('click', handleClickOutside);
         };
     })
 
@@ -43,7 +43,7 @@ export default function CookPage() {
     };
 
     const handleRemoveAll = () => {
-        setAllIngredients(prev=>prev.map(category=>({...category,selected:[]})))
+        setAllIngredients(prev => prev.map(category => ({ ...category, selected: [] })))
         setRemoveAllOpen(false)
     }
 
@@ -106,15 +106,15 @@ export default function CookPage() {
                                 { flex: isOpen }
                             )}
                         >
-                            {allIngredients.map((category,index) => (
+                            {allIngredients.map((category, index) => (
                                 <div key={index}>
-                                    {category.data.map((ingredient,i)=>(
-                                        ingredient.toLowerCase().includes(searchValue.toLowerCase()) && 
+                                    {category.data.map((ingredient, i) => (
+                                        ingredient.toLowerCase().includes(searchValue.toLowerCase()) &&
                                         <div className="text-black rounded-lg hover:bg-gray-100 py-2 w-full" key={i}>
-                                            <button 
-                                            onClick={()=>handleSelectedIngredients(ingredient,index)} 
-                                            className={cn(`text-start w-full px-2 bg-transparent`,
-                                            {'text-green-500':category.selected.includes(ingredient)})}
+                                            <button
+                                                onClick={() => handleSelectedIngredients(ingredient, index)}
+                                                className={cn(`text-start w-full px-2 bg-transparent`,
+                                                    { 'text-green-500': category.selected.includes(ingredient) })}
                                             >
                                                 {ingredient}
                                             </button>
@@ -128,34 +128,35 @@ export default function CookPage() {
                     <div className="flex flex-row-reverse justify-between">
                         <Button
                             className="bg-white text-black w-fit"
+                            bgColor="white"
                             onClick={() => setSwitchSearch(false)}
                         >
                             View Recipes
                         </Button>
                         <div className="flex items-center gap-1">
-                        <button 
-                        onClick={()=>setViewSelectedPage(prev=>!prev)} 
-                        className={cn('text-black relative rounded-lg transition-all border-black text-center duration-150 ease-in-out py-2 px-3 bg-white',{'scale-95':isViewSelectedPage})}>
-                            {
-                            <>
-                                <Box/>
-                                <div className="text-sm text-white absolute -top-2 bg-green-400 rounded-full px-2 -left-2">
-                                    {allIngredients.reduce((total,curValue)=>total + curValue.selected.length ,0)}
-                                </div> 
-                            </>
-                            }
-                            
-                        </button>
-                        {selectedIngredient.length > 0 && <button 
-                        onClick={()=>setRemoveAllOpen(true)} 
-                        className={cn('text-black relative rounded-lg transition-all text-center duration-150 ease-in-out py-2 px-3 bg-white',
-                        {'hidden':isRemoveAllOpen})}>
-                            Remove All
-                        </button>}
-                        <div className={cn('hidden gap-1 items-center',isRemoveAllOpen && 'flex')}>
-                            <button onClick={()=>setRemoveAllOpen(false)} className="bg-white rounded-lg text-black py-2 px-3">Cancel</button>
-                            <button onClick={handleRemoveAll} className="bg-red-500 py-2 px-3 rounded-lg text-white">Remove</button>
-                        </div>
+                            <button
+                                onClick={() => setViewSelectedPage(prev => !prev)}
+                                className={cn('text-black relative rounded-lg transition-all border-black text-center duration-150 ease-in-out py-2 px-3 bg-white', { 'scale-95': isViewSelectedPage })}>
+                                {
+                                    <>
+                                        <Box />
+                                        <div className="text-sm text-white absolute -top-2 bg-green-400 rounded-full px-2 -left-2">
+                                            {allIngredients.reduce((total, curValue) => total + curValue.selected.length, 0)}
+                                        </div>
+                                    </>
+                                }
+
+                            </button>
+                            {selectedIngredient.length > 0 && <button
+                                onClick={() => setRemoveAllOpen(true)}
+                                className={cn('text-black relative rounded-lg transition-all text-center duration-150 ease-in-out py-2 px-3 bg-white',
+                                    { 'hidden': isRemoveAllOpen })}>
+                                Remove All
+                            </button>}
+                            <div className={cn('hidden gap-1 items-center', isRemoveAllOpen && 'flex')}>
+                                <button onClick={() => setRemoveAllOpen(false)} className="bg-white rounded-lg text-black py-2 px-3">Cancel</button>
+                                <button onClick={handleRemoveAll} className="bg-red-500 py-2 px-3 rounded-lg text-white">Remove</button>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -163,56 +164,56 @@ export default function CookPage() {
                     className={`h-full w-full bg-white p-2 rounded-b-lg rounded-t-2xl overflow-auto  lg:block`}
                 >
                     {
-                        isViewSelectedPage 
-                        ? 
-                        <div>hi</div>
-                        :
-                        <div className="mt-3 w-[95%] mx-auto flex flex-col gap-10 ">
-                        {allIngredients.map((item, i) => (
-                            <div className="shadow-xl rounded-xl " key={i}>
-                                <div className="border-b border-gray-500">
-                                    <div className=" flex items-center justify-center gap-3">
-                                        <Image
-                                            src={`/images/ingredients/${item.image}.png`}
-                                            width={10000}
-                                            height={10000}
-                                            alt="ingredient image"
-                                            className="w-[60px] h-[60px]"
-                                        />
-                                        <p className="text-center py-3 text-xl">{item.title}</p>
+                        isViewSelectedPage
+                            ?
+                            <div>hi</div>
+                            :
+                            <div className="mt-3 w-[95%] mx-auto flex flex-col gap-10 ">
+                                {allIngredients.map((item, i) => (
+                                    <div className="shadow-xl rounded-xl " key={i}>
+                                        <div className="border-b border-gray-500">
+                                            <div className=" flex items-center justify-center gap-3">
+                                                <Image
+                                                    src={`/images/ingredients/${item.image}.png`}
+                                                    width={10000}
+                                                    height={10000}
+                                                    alt="ingredient image"
+                                                    className="w-[60px] h-[60px]"
+                                                />
+                                                <p className="text-center py-3 text-xl">{item.title}</p>
+                                            </div>
+                                            <p className="text-center text-gray-600 text-xl py-1">
+                                                {" "}
+                                                {item.selected.length} / {item.data.length} Ingredients
+                                            </p>
+                                        </div>
+                                        <div className="flex gap-2 flex-wrap w-[95%] h-[250px] overflow-auto py-3 mx-auto">
+                                            {item.data.map((ingred, idx) => (
+                                                <button
+                                                    className={cn(
+                                                        ` px-3 py-1 rounded-md text-gray-500 bg-gray-200 `,
+                                                        item.selected.map(
+                                                            (item) => item === ingred && "text-white bg-green-500"
+                                                        )
+                                                    )}
+                                                    key={idx}
+                                                    onClick={() => handleSelectedIngredients(ingred, i)}
+                                                >
+                                                    {ingred}
+                                                </button>
+                                            ))}
+                                        </div>
                                     </div>
-                                    <p className="text-center text-gray-600 text-xl py-1">
-                                        {" "}
-                                        {item.selected.length} / {item.data.length} Ingredients
-                                    </p>
-                                </div>
-                                <div className="flex gap-2 flex-wrap w-[95%] h-[250px] overflow-auto py-3 mx-auto">
-                                    {item.data.map((ingred, idx) => (
-                                        <button
-                                            className={cn(
-                                                ` px-3 py-1 rounded-md text-gray-500 bg-gray-200 `,
-                                                item.selected.map(
-                                                    (item) => item === ingred && "text-white bg-green-500"
-                                                )
-                                            )}
-                                            key={idx}
-                                            onClick={() => handleSelectedIngredients(ingred, i)}
-                                        >
-                                            {ingred}
-                                        </button>
-                                    ))}
-                                </div>
+                                ))}
                             </div>
-                        ))}
-                        </div>
                     }
-                </div>    
+                </div>
             </div>
             <div
                 className={`lg:flex flex flex-col gap-1  bg-gray-200 w-full h-full  rounded-lg ${switchSearch ? "hidden" : "block"
                     }`}
             >
-                <RecipePage setSwitchSearch={setSwitchSearch} />
+                <RecipesPage setSwitchSearch={setSwitchSearch} />
             </div>
         </main>
     );
