daf554f24d00a3c4ef90acf207794304667b28eb
LyhuaMam <144458914+LyhuaMam@users.noreply.github.com>
2024-01-23 22:14:06 +0700

    mobile menu links

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -8,15 +8,12 @@ import { Links } from "@/constant/const";
 import { cn } from "@/lib/utils";
 
 export default function Menu() {
+  const [isOpen, setIsOpen] = useState(-1);
 
-  const [isOpen,setIsOpen] = useState(-1)
-
-  const handleSelect = (n:number) => {
-    if(n === isOpen)
-      setIsOpen(-1)
-    else
-      setIsOpen(n)
-  }
+  const handleSelect = (n: number) => {
+    if (n === isOpen) setIsOpen(-1);
+    else setIsOpen(n);
+  };
 
   return (
     <Drawer>
@@ -31,27 +28,59 @@ export default function Menu() {
             </DrawerClose>
           </div>
           <div className="flex text-3xl h-screen px-5 w-full flex-col gap-4">
-            {Links.map((link,index)=>(
-              link.subCategory 
-              ? 
-              <button onClick={()=>handleSelect(index)} key={index} className="transition-all text-start duration-100 ease-in-out">
-                <div className="flex items-center gap-2">
-                  {link.name}
-                  <svg className={cn("transition-all text-start duration-100 ease-in-out",{"rotate-180":index === isOpen})} viewBox="0 0 8 6" height={12} width={14}>
-                  <path d="M4 6L0.535898 0L7.4641 0L4 6Z" fill="currentColor"></path>
-                  </svg>
-                </div>
-                <div className={cn("max-h-0 overflow-hidden flex flex-col gap-5",{'max-h-fit my-1 py-3 mx-5' : isOpen === index})}>
-                    {link.subCategory.map(sub=>(
-                      <Link className="hover:underline underline-offset-8 text-2xl decoration-4" key={sub} href={`${link.name === 'Cuisines' ? 'cuisine' : 'mealtype'}/${sub}`}>{sub}</Link>
+            {Links.map((link, index) =>
+              link.subCategory ? (
+                <button
+                  onClick={() => handleSelect(index)}
+                  key={index}
+                  className="transition-all text-start duration-100 ease-in-out"
+                >
+                  <div className="flex items-center gap-2">
+                    {link.name}
+                    <svg
+                      className={cn(
+                        "transition-all text-start duration-100 ease-in-out",
+                        { "rotate-180": index === isOpen }
+                      )}
+                      viewBox="0 0 8 6"
+                      height={12}
+                      width={14}
+                    >
+                      <path
+                        d="M4 6L0.535898 0L7.4641 0L4 6Z"
+                        fill="currentColor"
+                      ></path>
+                    </svg>
+                  </div>
+                  <div
+                    className={cn(
+                      "max-h-0 overflow-hidden flex flex-col gap-5",
+                      { "max-h-fit my-1 py-3 mx-5": isOpen === index }
+                    )}
+                  >
+                    {link.subCategory.map((sub) => (
+                      <Link
+                        className="hover:underline underline-offset-8 text-2xl decoration-4"
+                        key={sub}
+                        href={`/${
+                          link.name === "Cuisines" ? "cuisine" : "mealtype"
+                        }/${sub}`}
+                      >
+                        {sub}
+                      </Link>
                     ))}
-                </div>
-              </button>
-              :
-              <Link href={link.path} key={index} className="hover:opacity-75 font-normal transition-all duration-100 ease-in-out">
-                {link.name}
-              </Link>
-            ))}
+                  </div>
+                </button>
+              ) : (
+                <Link
+                  href={link.path}
+                  key={index}
+                  className="hover:opacity-75 font-normal transition-all duration-100 ease-in-out"
+                >
+                  {link.name}
+                </Link>
+              )
+            )}
           </div>
         </div>
       </DrawerContent>
--- components/PopularRecipe.tsx
+++ components/PopularRecipe.tsx
@@ -1,80 +1,84 @@
-'use client'
+"use client";
 
-import { Swiper, SwiperSlide } from 'swiper/react';
-import React, { useState } from 'react'
+import { Swiper, SwiperSlide } from "swiper/react";
+import React, { useState } from "react";
 
-import 'swiper/css';
-import 'swiper/css/navigation'
-import { Autoplay, Navigation } from 'swiper/modules';
-import FoodCard from './FoodCard';
+import "swiper/css";
+import "swiper/css/navigation";
+import { Autoplay, Navigation } from "swiper/modules";
+import FoodCard from "./FoodCard";
 
-const PopularFoods = [{
+const PopularFoods = [
+  {
     id: 633352,
-    name: 'Bacon Wrapped Tofu Tacos',
-    img: '/images/popular/bacon-wrapped-tacos.jpg',
-    cook: '25',
-},
-{
+    name: "Bacon Wrapped Tofu Tacos",
+    img: "/images/popular/bacon-wrapped-tacos.jpg",
+    cook: "25",
+  },
+  {
     id: 641687,
-    name: 'Dry Mee Siam',
-    img: '/images/popular/dry-mee-siam.jpg',
-    cook: '15',
-},
-{
+    name: "Dry Mee Siam",
+    img: "/images/popular/dry-mee-siam.jpg",
+    cook: "15",
+  },
+  {
     id: 661126,
-    name: 'Spicy Lump Crab and Avocado Salad',
-    img: '/images/popular/spicy-lamb.jpg',
-    cook: '1h 25',
-},
-{
+    name: "Spicy Lump Crab and Avocado Salad",
+    img: "/images/popular/spicy-lamb.jpg",
+    cook: "1h 25",
+  },
+  {
     id: 665537,
-    name: 'Yoghurt Honey Madeleine',
-    img: '/images/popular/yoghurt.jpg',
-    cook: '10',
-},
-{
+    name: "Yoghurt Honey Madeleine",
+    img: "/images/popular/yoghurt.jpg",
+    cook: "10",
+  },
+  {
     id: 638235,
-    name: 'Chicken Parmesan With Pasta',
-    img: '/images/popular/chicken-pasta.jpg',
-    cook: '20',
-},
-]
+    name: "Chicken Parmesan With Pasta",
+    img: "/images/popular/chicken-pasta.jpg",
+    cook: "20",
+  },
+];
 
 export default function PopularRecipe() {
+  const [slides, setSlides] = useState(1);
 
-    const [slides, setSlides] = useState(1)
+  const setSlidesPerview = () => {
+    setSlides(window.innerWidth <= 1280 ? 1 : 2);
+  };
 
-    const setSlidesPerview = () => {
-        setSlides(
-            window.innerWidth <= 1280 ? 1 : 2
-        );
-    }
+  React.useEffect(() => {
+    setSlidesPerview();
+    window.addEventListener("resize", setSlidesPerview);
+    return () => {
+      window.removeEventListener("resize", setSlidesPerview);
+    };
+  }, []);
 
-    React.useEffect(() => {
-        setSlidesPerview();
-        window.addEventListener("resize", setSlidesPerview);
-        return () => {
-            window.removeEventListener("resize", setSlidesPerview);
-        };
-    }, []);
-
-    return (
-        <Swiper
-            modules={[Navigation, Autoplay]}
-            navigation
-            autoplay
-            loop
-            slidesPerView={slides}
-            spaceBetween={'10px'}
-            className="w-[80%]">
-            {
-                PopularFoods.map((food: any) => (
-                    <SwiperSlide key={food.title} className='flex items-center justify-center py-3'>
-                        <FoodCard img={food.img} name={food.name} cook={food.cook} id={food.id} />
-                    </SwiperSlide>
-                ))
-            }
-
-        </Swiper>
-    )
+  return (
+    <Swiper
+      modules={[Navigation, Autoplay]}
+      navigation
+      autoplay
+      loop
+      slidesPerView={slides}
+      spaceBetween={"10px"}
+      className="w-[80%]"
+    >
+      {PopularFoods.map((food: any) => (
+        <SwiperSlide
+          key={food.name}
+          className="flex items-center justify-center py-3"
+        >
+          <FoodCard
+            img={food.img}
+            name={food.name}
+            cook={food.cook}
+            id={food.id}
+          />
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
 }
